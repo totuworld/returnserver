41013f302d9523408b4897bf4bec844207b4fd73
totuworld <totuworld@gmail.com>
2016-03-17 16:49:06 +0900

    update

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT=process.env.PORT || 3000;
 console.log(PORT);
 
 //targetpath는 원하는 주소로 변경합니다.
-let targetPath = '/.well-known/acme-challenge/ugn6HlGHzE5NBsvrfYCDscD0l35oatKnROdFXWv3HIg';
+let targetPath = '/.well-known/acme-challenge/hjcbhLr2TTcbTywrWesONhOoiNbKi3GpyUYksHDFRjA';
 //returnValue는 돌려줘야하는 값을 넣습니다.
-let returnValue = 'ugn6HlGHzE5NBsvrfYCDscD0l35oatKnROdFXWv3HIg.IBkQXVLbjkDk12yEvZSRNwSrUgZx3j23HgqTrqwP1KU'; 
+let returnValue = 'hjcbhLr2TTcbTywrWesONhOoiNbKi3GpyUYksHDFRjA.IBkQXVLbjkDk12yEvZSRNwSrUgZx3j23HgqTrqwP1KU'; 
 
 function handleRequest(request, response){
     try {
